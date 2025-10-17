f3c50f2da67b8e4665f47d6957b0b63950c08788
nicolas oses <nicoalexanderdev@gmail.com>
2025-10-17 16:43:13 -0300

    add modelo yolo project

---
+++ App MetroSence/modelo_yolo/README.md
@@ -0,0 +1,2 @@
+COMANDO BUILD DOCKER:  docker build -t modelo-yolo:latest .
+COMANDO RUN DOCKER:    docker run --rm -it -v "${PWD}:/app" modelo-yolo:latest
---
+++ App MetroSence/modelo_yolo/main.py
@@ -0,0 +1,14 @@
+from ultralytics import YOLO
+
+# Load the YOLO11 model
+model = YOLO("yolo11n.pt")
+
+# Export the model to TFLite format
+model.export(format="tflite")  # creates 'yolo11n_float32.tflite'
+
+# Load the exported TFLite model
+tflite_model = YOLO("yolo11n_saved_model/yolo11n_float32.tflite")
+
+# Run inference
+results = tflite_model("https://ultralytics.com/images/bus.jpg")
+print(results)
